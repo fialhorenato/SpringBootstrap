3fad15cd31f3711140f45539312c358aa270194c
Renato Fialho <renato.fialho@hotmart.com>
2022-04-20 14:25:28 +0200

    Improvements

--- src/test/kotlin/com/renato/springbootstrap/SpringBootstrapApplicationUnitTests.kt
+++ src/test/kotlin/com/renato/springbootstrap/SpringBootstrapApplicationUnitTests.kt
@@ -1,9 +1,8 @@
 package com.renato.springbootstrap
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-class SpringbootstrapApplicationUnitTests {
+class SpringBootstrapApplicationUnitTests {
 
 	@Test
 	fun contextLoads() {
