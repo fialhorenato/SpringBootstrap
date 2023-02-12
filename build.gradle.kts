f80f5e3c6a31dc53b4459a687e23c088a72e7b29
Renato Fialho <482565+fialhorenato@users.noreply.github.com>
2023-02-12 14:14:56 +0100

    Improving dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
 	id ("de.undercouch.download") version "3.4.3"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
-	kotlin("plugin.jpa") version "1.7.22"
+	kotlin("jvm") version "1.8.10"
+	kotlin("plugin.spring") version "1.8.10"
+	kotlin("plugin.jpa") version "1.8.10"
 	jacoco
 }
 
