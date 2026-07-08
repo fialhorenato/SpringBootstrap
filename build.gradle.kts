c092d936054ad1da95842d9feb6bad06d7aeff7e
Renato Fialho <482565+fialhorenato@users.noreply.github.com>
2026-07-08 15:20:17 +0200

    Removing vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,6 @@ version = "0.1.0"
 val nimbusJose4jVersion = "10.9.1"
 val springDocVersion = "3.0.3"
 val assertjVersion = "3.27.7"
-val fasterxmlJacksonVersion = "2.17.0"
-val toolsJacksonVersion = "3.2.0"
 val jacocoToolVersion = "0.8.13"
 
 kotlin {
